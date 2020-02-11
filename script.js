be33b46901d90011e9cbb74fc084b0a0077bd4dd
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-02-11 12:43:08 -0600

    third change

--- script.js
+++ script.js
@@ -1,30 +1,87 @@
 
-    // var getURL ="https://www.omdbapi.com/?apikey=5acb189&s=batman"
+$("#searchButton").click(function (event) {
+
+    event.preventDefault();
+
+    var movie = $("#searchform");
+    moviename = movie.val();
+    moviereview(moviename);
 
 
-   // Cors-anywhere.herokuapp.com
 
-    jQuery.ajaxPrefilter(function(options) {
-        if (options.crossDomain && jQuery.support.cors) {
-            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-        }
-    });
+});
 
-    
-    // var getURL ="https://www.goodreads.com/search.xml?key=MhYkPc3CNy5bvCefYOPkA&q=lordoftheflies"
-    // var getURL="https://www.goodreads.com/search.xml&key=MhYkPc3CNy5bvCefYOPkA&q=Ender%27s+Game"
 
-    var getURL="https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=batman&api-key=te0rQgoM9vfhltspkKfvFU0wZcAzmRL2"
 
-    // var getURL="https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyAqjuuGWZi_vl-ABKvzsasDkzTMbT3Rrds&part=snippet,contentDetails,statistics,status"
-    
+// Cors-anywhere.herokuapp.com
+
+jQuery.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+    }
+});
+
+
+// Look up movie review give movie name from form
+
+function moviereview(moviename) {
+
+    var getURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=" + moviename + "&api-key=te0rQgoM9vfhltspkKfvFU0wZcAzmRL2"
+
+
     $.ajax({
         url: getURL,
-        method:'GET'
+        method: 'GET'
         }).then(function (response) {
         console.log(response)
-        console.log(response.results[0].link.url)
-        console.log(response.results[0].link.suggested_link_text)
+
         console.log(response.results[0].headline)
-        
-        });
+
+        console.log(response.results[0].link.suggested_link_text)
+        console.log(response.results[0].link.url)
+
+
+
+        // set number of review to five   
+        var numberOfReview = 5
+        console.log(response.results.length)
+        // var length = response.results.length;
+
+        // set number of review equal to lenght if there are less than 5 reviews
+        //if (numberOfReview > length) {
+        //numberOfReview = length
+        //}
+
+        var reviewDiv = $("<h4>" + "Reviews : " + "</h4>")
+        var reviewcontainer = $("reviewcontainer")
+        reviewcontainer.append(reviewDiv)
+
+
+        for (var i = 0; i < numberOfReview; i++) {
+            var headlineDiv = $("<div>")
+
+            headlineDiv.addClass("headline")
+
+            reviewcontainer.text(respone.results[i].headline);
+
+            var suggestedDiv = $("<div>")
+
+            suggestedDiv.addClass("suggestedLink")
+
+            reviewcontainer.text(respone.results[i].link.suggested_link_text);
+
+            var urlDiv = $("<div>")
+
+            urlDiv.addClass("url")
+
+            reviewcontainer.append(urlDiv)
+
+            urlDiv.text("<a href='" + response.results[i].link.url + "'>" + response.results[i].link.url + "</a>");
+
+
+        }
+
+   });
+
+}
+
